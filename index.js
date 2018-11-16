5d167d0282878862a02c5e649b68324181f9fcb7
sameer9314 <sameersaurabh9314@gmail.com>
2018-11-16 11:01:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'sameersaurabh9314@gmail.com',
+    pass: '9135383190@sS'
+  }
+});
+
+var mailOptions = {
+  from: 'sameersaurabh9314@gmail.com',
+  to: 'sameersaurabh9314@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
