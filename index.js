a0ab1aa1f677f324aaf9fba6f734209b83c70c1e
SAURABH <sameer.saurabh@capgemini.com>
2018-11-20 12:45:38 +0530

    added screen shot

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'sameersaurabh9314@gmail.com',
-    pass: ''
+    pass: '************'
   }
 });
-
+var app = require("node-server-screenshot");
+app.fromURL("https://dcpcapgemini.cf/", "test.png", function(){
+    //an image of google.com has been saved at ./test.png
+    console.log('image saved');
+});
 var mailOptions = {
   from: 'sameersaurabh9314@gmail.com',
-  to: 'sameersaurabh9314@gmail.com',
+  to: 'sameer.saurabh@capgemini.com',
   subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+  text: 'That was easy!',
+  html: '<h1>Welcome</h1><p>That was easy!</p>',
+  attachments:[{
+    fiename:"test.jpg",
+     path:'/test.png'
+  }
+  ]
 };
 
 transporter.sendMail(mailOptions, function(error, info){
